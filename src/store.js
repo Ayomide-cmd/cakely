84320c68909c7bbda9c35f6dc10fa61f4d05dfff
Ayomide-cmd <185790396+Ayomide-cmd@users.noreply.github.com>
2026-01-15 22:12:20 +0100

    fix: resolve case-sensitive paths and file extensions

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice.js';
-
-import cartReducer from './features/cart/cartSlice';
+import cartReducer from './features/cart/cartSlice.js';
 
 const store = configureStore({
   reducer: {
